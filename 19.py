54f4185b456d25efe108727ef4ccd4b8c96501bf
Moritz Hahn <Moritz@hahnimnetz.de>
2022-10-20 22:50:15 +0200

    added problem #19

---
+++ 19.py
@@ -0,0 +1,34 @@
+from lib2to3.pgen2.token import ISTERMINAL
+import math
+
+def istSchaltjahr(a):
+    if ((a%4)==0 or (a%400)==0) and (a%100!=0):
+        return True
+    return False
+def ninetheen(y):
+    sunns=0
+    d=0
+    u=2
+    for i in range(1901,y):
+        for j in range(1,13):
+            if(j==4 or j==6 or j==9 or j==11):
+                d=30
+            elif(j==2):
+                if(istSchaltjahr(i)):
+                    d=29
+                else:
+                    d=28
+            else:
+                d=31
+            print(i,j,d)
+            for k in range(1,d+1):
+                if(u==7 and k==1):
+                    sunns+=1
+                if(u==7):
+                    u=1
+                else:
+                    u+=1
+                print(u)
+    print(sunns)
+            
+ninetheen(2001)
